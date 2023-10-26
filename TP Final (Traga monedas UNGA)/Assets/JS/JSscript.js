3825c17c77eedc5071156e347284c6cea4a46402
Acruxu <giuliodori.juancruz03@gmail.com>
2023-10-26 10:53:10 -0300

    cambio

--- TP Final (Traga monedas UNGA)/Assets/JS/JSscript.js
+++ TP Final (Traga monedas UNGA)/Assets/JS/JSscript.js
@@ -3,7 +3,8 @@ var oculto = document.getElementById("oculto"); // la seccion oculta del minijue
 var usuario = document.getElementById("usuario"); //guardamos el div donde se pondria el nombre del usuario
 var plata =  document.getElementById("plata"); //guardamos el div donde se pondria la plata del usuario
 var nombre = "";
-
+var botondinero = document.getElementById("apuesta");
+var pozo = 0;
 
 
 
@@ -16,6 +17,11 @@ oculto.style.display="block";
 botonini.style.display="none";
 nombre = prompt("ingrese su nombre");
 usuario.innerHTML = (nombre.toUpperCase());
+}
+botondinero.onclick = function(){
+pozo = parseInt(prompt("Ingrese un monto (debe ser un minimo de 100"));
+plata.innerHTML = (pozo);
+botondinero.innerHTML = ("Agregar saldo");
 }
 
 
