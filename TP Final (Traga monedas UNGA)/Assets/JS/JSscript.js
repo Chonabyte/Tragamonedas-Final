a9c3e1213f8a08fa3f929f1f9b557f61e14bb7cf
Acruxu <giuliodori.juancruz03@gmail.com>
2023-10-26 09:37:24 -0300

    actu

--- TP Final (Traga monedas UNGA)/Assets/JS/JSscript.js
+++ TP Final (Traga monedas UNGA)/Assets/JS/JSscript.js
@@ -1,4 +1,18 @@
 var botonini = document.getElementById("iniciar");
+var oculto = document.getElementById("oculto");
+botonini.onclick = function(){
+oculto.style.display="block";
+botonini.style.display="none";
+}
 
 
 
+
+
+
+
+
+
+
+
+//Funciones
