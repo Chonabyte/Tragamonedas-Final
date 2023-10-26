b5f301ed5a24e665b9c5b6ec727392c618028821
Acruxu <giuliodori.juancruz03@gmail.com>
2023-10-26 10:35:07 -0300

    Actu

--- TP Final (Traga monedas UNGA)/Assets/JS/JSscript.js
+++ TP Final (Traga monedas UNGA)/Assets/JS/JSscript.js
@@ -1,8 +1,21 @@
-var botonini = document.getElementById("iniciar");
-var oculto = document.getElementById("oculto");
+var botonini = document.getElementById("iniciar"); //boton comenzar
+var oculto = document.getElementById("oculto"); // la seccion oculta del minijuego
+var usuario = document.getElementById("usuario"); //guardamos el div donde se pondria el nombre del usuario
+var plata =  document.getElementById("plata"); //guardamos el div donde se pondria la plata del usuario
+var nombre = "";
+
+
+
+
+
+
+
+
 botonini.onclick = function(){
 oculto.style.display="block";
 botonini.style.display="none";
+nombre = prompt("ingrese su nombre");
+usuario.innerHTML = (nombre.toUpperCase());
 }
 
 
